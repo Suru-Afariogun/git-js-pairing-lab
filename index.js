43ff6e22e7623426a2a33651e63aa0429d1b3339
Suru-Afariogun <suruafa@gmail.com>
2024-10-03 17:20:42 -0400

    Answer added

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 //Code your solutions in this file
+//Q1
 const fiveToOneHundred = () => {
   for (let i = 5; i<101; i++) { 
     console .log(i)
     } 
 }
  fiveToOneHundred()
- 
+//Q2
+ const multiplesOfThree = () => {
+    for (let i = 0; i<101; i++){
+    if (i % 3 === 0 && i % 2 === 0) {
+    console.log(i)
+    }
+    }
+ }
+ multiplesOfThree()
+//Q3
+ const multiplesOfThreeOrFive = () => {
+    for (let i = 0; i<101; i++){
+        if (i % 3 === 0 || i % 5 === 0) {
+        console.log(i)
+        }
+        }
+ }
+
